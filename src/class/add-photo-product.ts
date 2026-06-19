19d853ab98145c0e106b4ef4480d819b92e491db
unknown <evertonjose421@gmail.com>
2026-06-19 17:54:44 -0300

    19/06 17:54

--- src/class/add-photo-product.ts
+++ src/class/add-photo-product.ts
@@ -12,7 +12,13 @@ export class AddPhotoProduct {
 
 
     static async add(PathPhotosErp:string){
-    const SQL = `SELECT CODIGO, OUTRO_COD, NUM_ORIGINAL  FROM ${db_publico}.cad_prod WHERE ATIVO = 'S' AND NUM_ORIGINAL <> '' AND NUM_ORIGINAL IS NOT NULL ;`
+    const SQL = `SELECT CODIGO, OUTRO_COD, NUM_ORIGINAL  FROM ${db_publico}.cad_prod
+     WHERE 
+     ATIVO = 'S'
+      AND NUM_ORIGINAL <> ''
+       AND NUM_ORIGINAL IS NOT NULL
+       AND CODIGO = 31537
+       ;`
 
       const [ resultProducts ] = await conn2.query(SQL)  
 
@@ -34,26 +40,26 @@ export class AddPhotoProduct {
 
      for(const product  of products){
         console.log(`[V] Verificando produto ${product.CODIGO}.`);
-          const photosFolder = datafolder.filter(( i )=>{ 
+        
+        const photosFolder = datafolder.filter(( i )=>{ 
                      if(i.startsWith(`${product.NUM_ORIGINAL}-`)   ){
                          return i;
                      };
                  })
- 
+                 
                         for(const photo of photosFolder){
-                            
-                            const [ resultMaxSequenc ] = await conn2.query(`SELECT MAX(SEQ) SEQ  FROM ${db_publico}.fotos_prod where PRODUTO = '${product.CODIGO}';`);
-                            const maxSequenc = resultMaxSequenc as [{SEQ:number}]      
-                            const seq = maxSequenc[0].SEQ && maxSequenc[0].SEQ > 0  ? maxSequenc[0].SEQ + 1 : 1 
-
+                             
                           const [ resultVerifyPhotoProduct ] = await conn2.query(`SELECT SEQ ,PRODUTO, FOTO FROM ${db_publico}.fotos_prod 
                            where PRODUTO = '${product.CODIGO}' AND FOTO = '${photo}';`);
 
                          const arrVerifyPhoto = resultVerifyPhotoProduct as resultVerifyPhoto[];
                             if(arrVerifyPhoto.length > 0 ){
-                                console.log(`Foto ${photo} já foi registrada.`)
+                                console.log(`[V] Foto ${photo} já foi registrada.`)
                             }else{
-                                
+                                     const [ resultMaxSequenc ] = await conn2.query(`SELECT MAX(SEQ) SEQ  FROM ${db_publico}.fotos_prod where PRODUTO = '${product.CODIGO}';`);
+                            const maxSequenc = resultMaxSequenc as [{SEQ:number}]      
+                            const seq = maxSequenc[0].SEQ && maxSequenc[0].SEQ > 0  ? maxSequenc[0].SEQ + 1 : 1 
+
                                 const sqlInsert = `INSERT INTO ${db_publico}.fotos_prod set produto = '${product.CODIGO}', 
                                     seq = '${seq}', descricao = '${product.NUM_ORIGINAL}', FOTO = '${photo}';`;
 
@@ -62,6 +68,24 @@ export class AddPhotoProduct {
                             }
                              
                          }
+
+                       const [ resultVerifyPhotoProduct ] = await conn2.query(`SELECT SEQ ,PRODUTO, 
+                        cast(FOTO  AS CHAR(10000) CHARACTER SET latin1 ) as FOTO
+                          FROM ${db_publico}.fotos_prod  
+                      where PRODUTO = '${product.CODIGO}' ;`);
+                     
+                      const arrVerifyPhoto = resultVerifyPhotoProduct as resultVerifyPhoto[];
+                     // console.log(arrVerifyPhoto)
+                                 for(const photo of arrVerifyPhoto ){
+                                     if(photo.FOTO.startsWith(`${product.NUM_ORIGINAL}-`) ){
+                                         console.log(`[V] Foto ${photo.FOTO} é valida para o produto ${product.CODIGO}`)
+                                     }else{
+                                         console.log(`[V] Foto ${photo.FOTO} invalida para o produto ${product.CODIGO}, efetuando a exclusão`)
+                                        await conn2.query(`DELETE FROM ${db_publico}.fotos_prod WHERE PRODUTO = '${photo.PRODUTO}' AND SEQ = '${photo.SEQ}'   `)
+
+                                     }
+                                 }
+
                          processed.push(product),
                          console.log(`[V] Processando ${processed.length} de ${products.length} ...`)
             }
--- src/job/photo-job.ts
+++ src/job/photo-job.ts
@@ -29,7 +29,7 @@ export class JobPhotos{
                 inExec = false;
             }
             
-        });
+       });
         
     }
 }
